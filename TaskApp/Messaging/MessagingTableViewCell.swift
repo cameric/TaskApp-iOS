d5573b19be6e7f012f34ef87c865fbe2bfe05ec1
Cam Zhou <Cam@Weiyus-MacBook.local>
2016-03-22 01:06:15 -0700

    messaging cell added

---
+++ TaskApp/Messaging/MessagingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessagingTableViewCell.swift
+//  TaskApp
+//
+//  Created by Cam on 3/22/16.
+//  Copyright © 2016 Cameric. All rights reserved.
+//
+
+import UIKit
+
+class MessagingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
